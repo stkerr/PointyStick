b86deaf08ac3a1aaa089e44dd41f91fca66c6cd4
kerrs_000 <kerrs_000>
2013-12-22 17:43:01 +0000

    Start on Windows events

--- tools/PINtool/src/events.cpp
+++ tools/PINtool/src/events.cpp
@@ -1,10 +1,73 @@
 #include "events.h"
+#include <stdio.h>
 
 static bool event_monitoring = false;
 static bool event_snapshot = false;
 
 #if defined(TARGET_WINDOWS)
-#error Windows events not implemented
+#include <Windows.h>
+static HANDLE lock_monitoring;
+static HANDLE lock_snapshot;
+
+bool initialize_events()
+{
+    lock_monitoring = CreateEventA(0, 0, 0, "MONITORING");
+    if (!lock_monitoring)
+    {
+        fprintf(stderr, "Error %d\n", GetLastError());
+        return false;
+    }
+
+    lock_snapshot = CreateEventA(0, 0, 0, "SNAPSHOT");
+    if (!lock_snapshot)
+    {
+        fprintf(stderr, "Error %d\n", GetLastError());
+        return false;
+    }
+
+    return true;
+}
+
+bool event_monitoring_enabled()
+{
+    fprintf(stderr, "Monitoring event unimplemented.\n");
+    return false;
+}
+
+void event_monitoring_set(bool status)
+{
+    if (status)
+        SetEvent(lock_monitoring);
+    else
+        ResetEvent(lock_monitoring);
+    
+    int error = GetLastError();
+    if (error)
+    {
+        fprintf(stderr, "Monitoring event error: %d\n", status);
+    }
+}
+
+bool event_snapshot_enabled()
+{
+    fprintf(stderr, "Snapshot event unimplemented.\n");
+    return false;
+}
+
+void event_snapshot_set(bool status)
+{
+    if (status)
+        SetEvent(lock_snapshot);
+    else
+        ResetEvent(lock_snapshot);
+
+    int error = GetLastError();
+    if (error)
+    {
+        fprintf(stderr, "Monitoring event error: %d\n", status);
+    }
+}
+
 
 #elif defined(TARGET_MAC) || defined(TARGET_LINUX)
 #include <semaphore.h>
--- tools/PINtool/src/snapshotting.cpp
+++ tools/PINtool/src/snapshotting.cpp
@@ -5,12 +5,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+#ifdef TARGET_WINDOWS
+#define snprintf _snprintf
+#endif
+
 bool take_snapshot(region_t *region)
 {
     time_t current_time = time(0);
 //    itoa(current_time, time_str, 10);
     
     char filename[260];
+    
     snprintf(filename, 260, "snapshot.%d.hex", (int)current_time);
 
     printf("%s\n", filename);
