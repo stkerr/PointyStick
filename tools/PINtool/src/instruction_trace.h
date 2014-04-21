677cb736e1fa6253ba5e7c26337d008f779c3984
Nicholas Niro <nik_89@xroutine.net>
2014-04-21 18:09:48 -0400

    PINtool instruction_trace: Fixed an issue in pin.H inclusion.

--- tools/PINtool/src/instruction_trace.h
+++ tools/PINtool/src/instruction_trace.h
@@ -1,7 +1,7 @@
 #ifndef ____instruction_trace__
 #define ____instruction_trace__
 
-#include "pin.H"
+#include <pin.H>
 void instruction_trace(INS ins, void* arg);
 
 #endif /* defined(____instruction_trace__) */
